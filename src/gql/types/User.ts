838412e884f97af6238d6ad3b2bfa3d4bac5558f
lostfictions <lostfictions@users.noreply.github.com>
2019-12-21 04:39:34 -0500

    editing schema

--- src/gql/types/User.ts
+++ src/gql/types/User.ts
@@ -4,8 +4,5 @@ export const User = objectType({
   name: "User",
   definition(t) {
     t.model.id();
-    t.model.name();
-    t.model.email();
-    t.model.posts({ pagination: false });
   }
 });
