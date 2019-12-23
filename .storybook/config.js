3bac64c7b4f2efcc42af1f7e87a836bd278f5d9b
lostfictions <lostfictions@users.noreply.github.com>
2019-12-23 02:35:25 -0500

    use own stylelint config

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,9 @@
 import { configure, addDecorator } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
-import { IntlProvider } from "react-intl";
 
 import "../src/styles/index.css";
 
 addDecorator(withInfo({ inline: true }));
-addDecorator(storyFn => <IntlProvider locale="en">{storyFn()}</IntlProvider>);
+addDecorator(storyFn => <div>{storyFn()}</div>);
 
 configure(require.context("../stories", true, /\.tsx?$/), module);
