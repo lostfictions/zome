751fb6691de014cc61a9fce2b543f33d415e3b48
lostfictions <lostfictions@users.noreply.github.com>
2019-12-24 00:54:33 -0500

    whitelisted guilds as env var for now

--- src/server/env.ts
+++ src/server/env.ts
@@ -1,9 +1,19 @@
-import { cleanEnv, str } from "envalid";
+import { cleanEnv, str, makeValidator } from "envalid";
+
+const strSet = makeValidator<Set<string>>(x => {
+  const arr = JSON.parse(x);
+  if (!Array.isArray(arr)) throw new Error("Must be an array!");
+  if (!arr.every(v => typeof v === "string")) {
+    throw new Error("Must be array of strings");
+  }
+  return new Set<string>(arr);
+});
 
 export const {
   DISCORD_CLIENT_SECRET,
   DISCORD_CLIENT_ID,
   APP_SECRET,
+  WHITELISTED_GUILDS,
   HOST
 } = cleanEnv(
   process.env,
@@ -11,6 +21,7 @@ export const {
     DISCORD_CLIENT_SECRET: str(),
     DISCORD_CLIENT_ID: str(),
     APP_SECRET: str(),
+    WHITELISTED_GUILDS: strSet(),
     HOST: str({ devDefault: "http://localhost:3000" })
   },
   { strict: true }
